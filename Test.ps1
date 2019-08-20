5fda14c80d372e89951eba82b6cb7618fc54b0dd
despingu <david.espin@outlook.com>
2019-08-20 15:59:35 +0200

    removed test.ps1

--- Test.ps1
+++
@@ -1,9 +0,0 @@
-$appId = "yourappid"
-$appSecret = "yoursecret"
-$TenantName = "yourtenant.onmicrosoft.com"
-
-Import-Module "PathToModule\GraphPS.psd1"
-Get-Module GraphPS
-Connect-GraphPS -TenantName $TenantName -AppID $appId -AppSecret $appSecret -Version beta
-Get-GraphPSConnectionInfo
-Get-GraphPSUsers -filterExpression "userType eq 'Guest'"
