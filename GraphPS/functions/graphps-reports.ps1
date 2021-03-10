e384a8e64785aad7507ac406c6b8adc5c9778aac
despingu <david.espin@outlook.com>
2021-03-10 13:31:56 +0100

    Added new report functions

--- GraphPS/functions/graphps-reports.ps1
+++ GraphPS/functions/graphps-reports.ps1
@@ -97,3 +97,47 @@ function Get-GraphPSCredentialUserRegistrationDetails {
     $graphResult = Invoke-MSGraphQuery -Endpoint $endpoint -FilterExpression $filterExpression -SelectExpression $selectExpression -FormatExpression $FormatExpression -Method GET
     return $graphResult
 }
+
+function Get-GraphPSGetMailboxUsageDetail {
+    param (
+        [Parameter(Mandatory=$TRUE)]
+        [ValidateSet("D7", "D30", "D90", "D180")]
+        [string]$Period,
+        [Parameter(Mandatory=$false)]
+        [string]$filterExpression,
+        [Parameter(Mandatory=$false)]
+        [string]$selectExpression,
+        [Parameter(Mandatory=$false)]
+        [string]$FormatExpression
+    )
+    if (-not (Test-Connection)) {
+        return
+    }
+
+    $endpoint = "reports/getMailboxUsageDetail(period='$Period')"
+
+    $graphResult = Invoke-MSGraphQuery -Endpoint $endpoint -FilterExpression $filterExpression -SelectExpression $selectExpression -FormatExpression $FormatExpression -Method GET
+    return $graphResult
+}
+
+function Get-GraphPSGetSharePointSiteUsageDetail {
+    param (
+        [Parameter(Mandatory=$TRUE)]
+        [ValidateSet("D7", "D30", "D90", "D180")]
+        [string]$Period,
+        [Parameter(Mandatory=$false)]
+        [string]$filterExpression,
+        [Parameter(Mandatory=$false)]
+        [string]$selectExpression,
+        [Parameter(Mandatory=$false)]
+        [string]$FormatExpression
+    )
+    if (-not (Test-Connection)) {
+        return
+    }
+
+    $endpoint = "reports/getSharePointSiteUsageDetail(period='$Period')"
+
+    $graphResult = Invoke-MSGraphQuery -Endpoint $endpoint -FilterExpression $filterExpression -SelectExpression $selectExpression -FormatExpression $FormatExpression -Method GET
+    return $graphResult
+}
